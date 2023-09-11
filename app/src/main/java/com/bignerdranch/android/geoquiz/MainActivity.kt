ddcd57999291d78cc8ea700c3d84f5151d189e9b
Michael Truong <truong.minh.michael@gmail.com>
2023-09-10 17:05:10 -0700

    display score for quiz after done

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -9,7 +9,8 @@ import com.bignerdranch.android.geoquiz.databinding.ActivityMainBinding
 import com.bignerdranch.android.geoquiz.databinding.TrueFalseButtonsBinding
 
 class MainActivity : AppCompatActivity() {
-
+    private var correctAnswers = 0
+    private var questionsAnswered = 0
     private lateinit var binding: ActivityMainBinding
     private lateinit var includedBinding: TrueFalseButtonsBinding
 
@@ -34,33 +35,39 @@ class MainActivity : AppCompatActivity() {
         // Set button click listeners for the included layout components
         includedBinding.trueButton.setOnClickListener { view: View ->
             checkAnswer(true)
-
+            this.questionsAnswered++
         }
 
         includedBinding.falseButton.setOnClickListener { view: View ->
             checkAnswer(false)
-
+            this.questionsAnswered++
         }
 
         binding.prevButton.setOnClickListener {
             currentIndex = (currentIndex - 1) % questionBank.size
             val questionTextResId = questionBank[currentIndex].textResId
             binding.questionTextView.setText(questionTextResId)
+            updateNavButtons()
         }
 
         binding.nextButton.setOnClickListener {
             currentIndex = (currentIndex + 1) % questionBank.size
             val questionTextResId = questionBank[currentIndex].textResId
             binding.questionTextView.setText(questionTextResId)
+            updateNavButtons()
         }
 
         val questionTextResId = questionBank[currentIndex].textResId
         binding.questionTextView.setText(questionTextResId)
 
+        updateNavButtons()
     }
 
     private fun checkAnswer(userAnswer: Boolean) {
         val correctAnswer = questionBank[currentIndex].answer
+        if (userAnswer == correctAnswer) {
+            this.correctAnswers++
+        }
         val messageResId = if (userAnswer == correctAnswer) {
             R.string.correct_toast
         } else {
@@ -68,6 +75,22 @@ class MainActivity : AppCompatActivity() {
         }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
             .show()
+
+        if (questionsAnswered == questionBank.size) {
+            val score = (correctAnswers.toDouble() / questionBank.size) * 100
+            Toast.makeText(this, "Quiz Score: ${correctAnswers} / ${questionBank.size}, ${String.format("%.2f", score)}%", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun updateNavButtons() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        binding.questionTextView.setText(questionTextResId)
+
+        // Set the visibility of the "Prev" button based on whether it's the first question
+        binding.prevButton.visibility = if (currentIndex == 0) View.INVISIBLE else View.VISIBLE
+
+        // Set the visibility of the "Next" button based on whether it's the last question
+        binding.nextButton.visibility = if (currentIndex == questionBank.size - 1) View.INVISIBLE else View.VISIBLE
     }
 
 }
